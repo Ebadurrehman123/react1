cee0b8376f8c3d326815a34c5a6ef227beb1b67b
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-07-20 22:54:07 +0500

    commit

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,13 +1,25 @@
 function customRender(reactElement,container){
+    /*
     const domElement=document.createElement
     (reactElement.type)
-    domElement.innerHtml=reactElement.children
+    domElement.innerHTML=reactElement.children
     domElement.setAttribute('href',reactElement.props.
     href)
     domElement.setAttribute('target',reactElement.props.
     target)
     container.appendChild(domElement)
+*/
+     const domElement=document.createElement
+     (reactElement.type)
+     domElement.innerHTML=reactElement.children
+     for(const prop in reactElement.props){
+        if (prop==='children')continue;
+        domElement.setAttribute(prop,reactElement.props
+        [prop])
+    }
+     container.appendChild(domElement)
 }
+    
 
 const reactElement={
     type:'a',
@@ -20,4 +32,6 @@ const reactElement={
 
 const mainContainer=document.querySelector("#root")
 
+//custom render do cheezay expect krta hai
+//pehli cheez kiya inject kru,or dusri cheez ka pr inject kru 
 customRender(reactElement,mainContainer)
