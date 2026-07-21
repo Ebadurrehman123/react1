3f5bf3edcdb33528bd7a3905dfaba6958c45c552
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-07-21 13:30:55 +0500

    commit

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App!</h1>
+    </div>
+  )
+}
+/*
+const reactElement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'__blank'
+    },
+    children:'Click me to visit google'
+}*/
+
+const anotherElement=(
+  <a href="https://google.com" target='blank'>Visit google</a>
+)
+const reactElement=React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'click me to visit google'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+   // anotherElement
+   //reactElement
+   <App/>
 )
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+//use state sa ap ko do cheezay milti hai array ki form ma
+//jo zero index pr value milti hai wo apko milti hai simply counter name kuch bhi hoskta hai
+//dusri value jo milti hai wo value nai hoti wo ik function hota hai
+//js ko usually setcounter boltay hai
+//dusra jo function hai wo reaposible hai fist variable ko 
+//update krna kay liya
+  let [counter,setcounter]=useState(15)
+
+
+ // let counter=15
+  const addValue=()=>{
+    console.log("Clicked",counter);
+    //counter=counter+1
+    setcounter(counter+1)
+  }
+  const removevalue=()=>{
+    setcounter(counter-1)
+  }
+  
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}>add value{counter}</button>
+      <br />
+      <button
+      onClick={removevalue}>remove value{counter}</button>
+      <p>footer:{counter}</p>
+    </>
+  )
+}
+
+export default App
