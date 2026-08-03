733f29ddae8f4a8abceaacc88a54d1f6756f7ca8
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-08-03 23:10:47 +0500

    commit

--- 08minicontext/src/App.jsx
+++ 08minicontext/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+
+    <UserContextProvider>
       <h1>React</h1>
-    </>
+    </UserContextProvider>
   )
 }
 
