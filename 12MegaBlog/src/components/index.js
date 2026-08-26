8586ef903a7740137231da02f69b867df6a71baf
Ebadurrehman123 <urrehmanebad01@gmail.com>
2026-08-26 22:07:09 +0500

    commit

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./header/Header";
+import Footer from "./Footer/Footer";
+import Container_ from ".container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
